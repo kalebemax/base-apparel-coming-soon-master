56b35402c8ee60760d681720baa2707357fb6314
kalebemax <169088614+kalebemax@users.noreply.github.com>
2024-08-28 15:46:13 -0300

    updated

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-    const btn = document.querySelector('#btn')
+const btn = document.querySelector('#btn')
 
-    const emailTest = () => {
-        const emailInput = document.getElementById('email')
-        const emailValue = emailInput.value;
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        const errorMessage = document.getElementById('txt');
-        const icon = document.getElementById('icon')
-        const inputContainer = document.getElementById('inputContainer')
-        const img = 'images/icon-error.svg'
+const emailTest = () => {
+    const emailInput = document.getElementById('email')
+    const emailValue = emailInput.value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const errorMessage = document.getElementById('txt');
+    const icon = document.getElementById('icon')
+    const inputContainer = document.getElementById('inputContainer')
+    const img = '/images/icon-error.svg'
 
-        if (!emailValue) {
-        inputContainer.style.borderColor = 'red'
-        icon.innerHTML = `<img src="${img}">`
-        errorMessage.textContent = "Email address is required.";
-    } else if (!emailRegex.test(emailValue)) {
-        inputContainer.style.borderColor = 'red'
-        icon.innerHTML = `<img src="${img}">`
-        errorMessage.textContent = "Please provide a valid email address.";
-    } else if (emailValue) {
-        inputContainer.style.borderColor = 'green'
-        icon.innerHTML = ''
-        errorMessage.textContent = ''
-    } 
+    if (!emailValue) {
+    inputContainer.style.borderColor = 'red'
+    icon.innerHTML = `<img src="${img}">`
+    errorMessage.textContent = "Email address is required.";
+} else if (!emailRegex.test(emailValue)) {
+    inputContainer.style.borderColor = 'red'
+    icon.innerHTML = `<img src="${img}">`
+    errorMessage.textContent = "Please provide a valid email address.";
+} else if (emailValue) {
+    inputContainer.style.borderColor = 'green'
+    icon.innerHTML = ''
+    errorMessage.textContent = ''
+} 
 }
-     btn.addEventListener('click', emailTest) 
+ btn.addEventListener('click', emailTest) 
+
+ document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        emailTest();
+    }
+});
 
